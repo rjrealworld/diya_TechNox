f3b9669d07a8262bcf2be9ebcec27879a3537bc1
Riya Jain <riyajn2001@gmail.com>
2021-04-18 02:59:31 +0530

    Added the back button in chatbot

--- diya/lib/chat_bot.dart
+++ diya/lib/chat_bot.dart
@@ -6,12 +6,8 @@ void main() => runApp(Chatbot());
 class Chatbot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Diya counsellor bot✨',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: Agent(title: 'Diya counsellor bot✨'),
+    return Scaffold(
+      body: Agent(title: 'Diya counsellor bot✨'),
     );
   }
 }
