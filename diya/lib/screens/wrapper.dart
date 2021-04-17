12514bf6edcdcee4b8fdb1d6c65b4bcdc0746c03
varshaav16 <sec19cs118@sairamtap.edu.in>
2021-04-18 00:55:20 +0530

    removed extra import stmnts

--- diya/lib/screens/wrapper.dart
+++ diya/lib/screens/wrapper.dart
@@ -1,12 +1,8 @@
 import 'package:diya/Homepage/main.dart';
-import 'package:diya/main.dart';
-import 'package:diya/screens/Dashboard/dashboard.dart';
 import 'package:diya/screens/Signup/signup_screen.dart';
-import 'package:diya/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:diya/models/user.dart';
-import 'package:diya/screens/Login/login_screen.dart';
 
 class Wrapper extends StatelessWidget {
   @override
