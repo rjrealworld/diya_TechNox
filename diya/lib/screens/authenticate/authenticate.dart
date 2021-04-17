36556a02c78df72ec6e251f51b042547075fb30a
varshaav16 <sec19cs118@sairamtap.edu.in>
2021-04-17 18:11:28 +0530

    linked with login

--- diya/lib/screens/authenticate/authenticate.dart
+++ diya/lib/screens/authenticate/authenticate.dart
@@ -1,3 +1,4 @@
+import 'package:diya/screens/Login/login_screen.dart';
 import 'package:diya/screens/authenticate/signIn.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,7 @@ class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: SignIn(),
+      child: LoginScreen(),
     );
   }
 }
